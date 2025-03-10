260db078f6e92ba4f033e7b53ecb3803619bbf9a
LBM <lukebm@duck.com>
2025-03-10 10:53:03 +0000

    Remap esc

--- init.lua
+++ init.lua
@@ -159,6 +159,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Remap `kj` to `<Esc>`
+vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
