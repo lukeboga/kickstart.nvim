855cdec3b6fa0a28ace380c3d0b41f2a3c3ab4e8
LBM <lukebm@duck.com>
2025-03-10 12:13:15 +0000

    Enable transparent BG

--- init.lua
+++ init.lua
@@ -900,6 +900,7 @@ require('lazy').setup({
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
+        transparent = true,
       }
 
       -- Load the colorscheme here.
